1410b5773c9fc54d040350a82463ecdd6891dd6d
left2right <yunqian_zhang@126.com>
2017-05-17 00:07:42 +0800

    codis kubernetes scripts

--- kubernetes/setup.sh
+++ kubernetes/setup.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
-### PRODUCT_NAME="codis-demo"
-### sed -i  "/name: CODIS_PRODUCT/{n;s/\".*\"/\"${PRODUCT_NAME}\"/}" codis-dashboard.yaml
-### sed -i  "/name: CODIS_PRODUCT/{n;s/\".*\"/\"${PRODUCT_NAME}\"/}" codis-proxy.yaml
+case "$1" in
 
 ### 清理原来codis遗留数据
-function cleanup() {
+cleanup)
     kubectl delete -f .
-    kubectl exec -it zk-0 -- zkCli.sh -server zk-0:2181 rmr /codis3/"codis-demo"
-}
+    # 登陆上zk机器上执行 zkCli.sh -server pro4-aws-ca-zk3:2181 rmr /codis3/codis-demo
+    kubectl exec -it zk-0 -- zkCli.sh -server zk-0:2181 rmr /codis3/codis-demo
+    ;;
 
 ### 创建新的codis集群
-function buildup() {
+buildup)
+    kubectl delete -f .
+    # 登陆上zk机器上执行 zkCli.sh -server pro4-aws-ca-zk3:2181 rmr /codis3/codis-demo
+    kubectl exec -it zk-0 -- zkCli.sh -server zk-0:2181 rmr /codis3/codis-demo
     kubectl create -f codis-service.yaml
     kubectl create -f codis-dashboard.yaml
+    while [ $(kubectl get pods -l app=codis-dashboard |grep Running |wc -l) != 1 ]; do sleep 1; done;
     kubectl create -f codis-proxy.yaml
     kubectl create -f codis-server.yaml
     servers=$(grep "replicas" codis-server.yaml |awk  '{print $2}')
-    while [ $(kubectl get pods -l app=codis-server -o name |wc -l) != $servers ]; do sleep 1; done;
+    while [ $(kubectl get pods -l app=codis-server |grep Running |wc -l) != $servers ]; do sleep 1; done;
     kubectl exec -it codis-server-0 -- codis-admin  --dashboard=codis-dashboard:18080 --rebalance --confirm
     kubectl create -f codis-ha.yaml
     kubectl create -f codis-fe.yaml
@@ -26,23 +29,14 @@ function buildup() {
     if [ $? != 0 ]; then
         echo "buildup codis cluster with problems, plz check it!!"
     fi
-}
-
-
-case "$1" in
-cleanup)
-    cleanup
     ;;
 
-buildup)
-    cleanup
-    buildup
-    ;;
-
+### 扩容／缩容 codis proxy
 scale-proxy)
     kubectl scale rc codis-proxy --replicas=$2
     ;;
 
+### 扩容／缩容 codis server
 scale-server)
     cur=$(kubectl get statefulset codis-server |tail -n 1 |awk '{print $3}')
     des=$2
@@ -52,7 +46,7 @@ scale-server)
         echo "current server == desired server, return"
     elif [ $cur < $des ]; then
         kubectl scale statefulsets codis-server --replicas=$des
-        while [ $(kubectl get pods -l app=codis-server -o name |wc -l) != $2 ]; do sleep 1; done;
+        while [ $(kubectl get pods -l app=codis-server |grep Running |wc -l) != $2 ]; do sleep 1; done;
         kubectl exec -it codis-server-0 -- codis-admin  --dashboard=codis-dashboard:18080 --rebalance --confirm
     else
         while [ $cur > $des ]
